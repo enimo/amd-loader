03b394ec445973e4443290ec67e6cf5c5af9a2f3
LUO <enimong@gmail.com>
2014-12-11 21:52:44 +0800

    enhance the test case

--- test/def.calcu.js
+++ test/def.calcu.js
@@ -1,4 +1,14 @@
-define(function() {
+(function(win, factory){
+    if (typeof define == 'function' && typeof define.amd == 'object') {
+        //win._ = factory();
+        define('test/calcu', factory);
+    }
+    else {
+        win.calcu = factory();
+    }
+//}.call(this));
+})(window, function(){ 
+
 	AMD.emit('done', 'from calcu');
 	return {
 	    add: function(x, y){
